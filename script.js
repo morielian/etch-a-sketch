bab710bef6870faf0e34be8bd4cccc164c260642
Douglas Purdy <morielian@gmail.com>
2022-10-29 19:58:33 -0400

    working on logic for board

---
+++ script.js
@@ -0,0 +1,20 @@
+function createBoard(size) {
+    let board = document.querySelector('.board');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove())
+
+    let amount = size * size
+    for(let i = 0; i < amount; i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
+
+createBoard(16);
+
+function changeSize(input){
+    createBoard(input);
+};
